61c9b2ff2a110393c66717671794da124c1fa6e5
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2017-12-29 13:37:22 +0800

    add notMatch matchCount decimal digital typein negative positive zreo integer

--- src/index.js
+++ src/index.js
@@ -8,17 +8,48 @@ const getType = data => {
   return str.toLowerCase();
 };
 
-const isBuffer = (value, msg) => {
+const isBuffer = (value, msg = '') => {
   if (!_.isBuffer(value)) {
     throw new Error(`${msg}: expected ${value} to be a buffer`);
   }
 };
-
-// const notEmpty = (value, msg) => {
-  // if (_.isEmpty(value)) {
-    // throw new Error(`${msg}: expected ${value} not to be a empty`);
-  // }
-// };
+const isInteger = (value, msg = '') => {
+  if (!Number.isInteger(value)) {
+    throw new Error(`${msg}: expected ${value} to be a integer`);
+  }
+};
+const isZreo = (value, msg = '') => {
+  if (value !== 0) {
+    throw new Error(`${msg}: expected ${value} to be a zreo`);
+  }
+};
+const isPositive = (value, msg = '') => {
+  if (value < 0) {
+    throw new Error(`${msg}: expected ${value} to be a positive number`);
+  }
+};
+const isNegative = (value, msg = '') => {
+  if (value > 0) {
+    throw new Error(`${msg}: expected ${value} to be a negative number`);
+  }
+};
+const decimal = (value, decimal, msg = '') => {
+  const d = String(value).split('.')[1].length;
+  if (d !== decimal) {
+    throw new Error(`${msg}: expected ${value} decimal to be ${decimal}`);
+  }
+};
+const isDigital = (value, msg = '') => {
+  if (isNaN(value)) {
+    throw new Error(`${msg}: expected ${value} to be digital`);
+  }
+};
+const matchCount = (value, reg, count, msg = '') => {
+  const result = value.match(reg)
+  if (result.length !== count) {
+    throw new Error(`${msg}: expected ${value} match ${reg} to ${count} result`);
+  }
+};
 
 class Assert {
   constructor() {
@@ -39,6 +70,17 @@ class Assert {
         argument[0] = argument[1];
         argument[1] = swop;
         fun(...argument);
+        break;
+      }
+      case 'typeOfIn': {
+        for (const value of argument[1]) {
+          try {
+            assert.typeOf(argument[0], value);
+            return this;
+          } catch {
+          }
+        }
+        throw new Error(`${argument[2]}: expected ${argument[0]}:${getType(argument[0])} type in [${argument[1]}]`);
         break;
       }
       default: {
@@ -131,6 +173,43 @@ class Assert {
       args.unshift('regexp');
     }
     return this.execute(assert.typeOf, ...args);
+  }
+  integer(...args) {
+    return this.execute(isInteger, ...args);
+  }
+  zreo(...args) {
+    return this.execute(isZreo, ...args);
+  }
+  positive(...args) {
+    return this.execute(isPositive, ...args);
+  }
+  negative(...args) {
+    return this.execute(isNegative, ...args);
+  }
+  typein(...args) {
+    return this.execute('typeOfIn', ...args);
+  }
+  digital(...args) {
+    return this.execute(isDigital, ...args);
+  }
+  decimal(...args) {
+    return this.execute(decimal, ...args);
+  }
+  notMatch(...args) {
+    if (_.isNil(this.value)) {
+      args[1] = getType(args[1]) === 'string' ? new RegExp(args[1], 'g') : args[1];
+    } else {
+      args[0] = getType(args[0]) === 'string' ? new RegExp(args[0], 'g') : args[0];
+    }
+    return this.execute(assert.notMatch, ...args);
+  }
+  matchCount(...args) {
+    if (_.isNil(this.value)) {
+      args[1] = getType(args[1]) === 'string' ? new RegExp(args[1], 'g') : args[1];
+    } else {
+      args[0] = getType(args[0]) === 'string' ? new RegExp(args[0], 'g') : args[0];
+    }
+    return this.execute(matchCount, ...args);
   }
 }
 export default new Assert();
