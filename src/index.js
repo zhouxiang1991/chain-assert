b58cd4b3cc65d53848505f8a0f628bf8b712b57c
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2017-12-29 14:13:19 +0800

    modify multi call assert instance erro bug

--- src/index.js
+++ src/index.js
@@ -52,7 +52,8 @@ const matchCount = (value, reg, count, msg = '') => {
 };
 
 class Assert {
-  constructor() {
+  constructor(first) {
+    this.first = first;
     this.value = null;
   }
   execute(fun, ...args) {
@@ -91,63 +92,123 @@ class Assert {
     return this;
   }
   include(...args) {
+    if (this.first) {
+      return new Assert().include(...args);
+    }
     return this.execute(assert.deepInclude, ...args);
   }
   gt(...args) {
+    if (this.first) {
+      return new Assert().gt(...args);
+    }
     return this.execute(assert.isAbove, ...args);
   }
   gte(...args) {
+    if (this.first) {
+      return new Assert().gte(...args);
+    }
     return this.execute(assert.isAtLeast, ...args);
   }
   lt(...args) {
+    if (this.first) {
+      return new Assert().lt(...args);
+    }
     return this.execute(assert.isBelow, ...args);
   }
   lte(...args) {
+    if (this.first) {
+      return new Assert().lte(...args);
+    }
     return this.execute(assert.isAtMost, ...args);
   }
   true(...args) {
+    if (this.first) {
+      return new Assert().true(...args);
+    }
     return this.execute(assert.isTrue, ...args);
   }
   equal(...args) {
+    if (this.first) {
+      return new Assert().equal(...args);
+    }
     return this.execute(assert.deepEqual, ...args);
   }
   unequal(...args) {
+    if (this.first) {
+      return new Assert().unequal(...args);
+    }
     return this.execute(assert.notDeepEqual, ...args);
   }
   false(...args) {
+    if (this.first) {
+      return new Assert().false(...args);
+    }
     return this.execute(assert.isFalse, ...args);
   }
   exists(...args) {
+    if (this.first) {
+      return new Assert().exists(...args);
+    }
     return this.execute(assert.exists, ...args);
   }
   notEmpty(...args) {
+    if (this.first) {
+      return new Assert().notEmpty(...args);
+    }
     return this.execute(assert.isNotEmpty, ...args);
   }
   function(...args) {
+    if (this.first) {
+      return new Assert().function(...args);
+    }
     return this.execute(assert.isFunction, ...args);
   }
   object(...args) {
+    if (this.first) {
+      return new Assert().object(...args);
+    }
     return this.execute(assert.isObject, ...args);
   }
   array(...args) {
+    if (this.first) {
+      return new Assert().array(...args);
+    }
     return this.execute(assert.isArray, ...args);
   }
   number(...args) {
+    if (this.first) {
+      return new Assert().number(...args);
+    }
     return this.execute(assert.isNumber, ...args);
   }
   bool(...args) {
+    if (this.first) {
+      return new Assert().bool(...args);
+    }
     return this.execute(assert.isBoolean, ...args);
   }
   null(...args) {
+    if (this.first) {
+      return new Assert().null(...args);
+    }
     return this.execute(assert.isNull, ...args);
   }
   string(...args) {
+    if (this.first) {
+      return new Assert().string(...args);
+    }
     return this.execute(assert.isString, ...args);
   }
   undefined(...args) {
+    if (this.first) {
+      return new Assert().undefined(...args);
+    }
     return this.execute(assert.isUndefined, ...args);
   }
   match(...args) {
+    if (this.first) {
+      return new Assert().match(...args);
+    }
     if (_.isNil(this.value)) {
       args[1] = getType(args[1]) === 'string' ? new RegExp(args[1], 'g') : args[1];
     } else {
@@ -156,9 +217,15 @@ class Assert {
     return this.execute(assert.match, ...args);
   }
   buffer(...args) {
+    if (this.first) {
+      return new Assert().buffer(...args);
+    }
     return this.execute(isBuffer, ...args);
   }
   date(...args) {
+    if (this.first) {
+      return new Assert().date(...args);
+    }
     if (_.isNil(this.value)) {
       args.splice(1, 0, 'date');
     } else {
@@ -167,6 +234,9 @@ class Assert {
     return this.execute(assert.typeOf, ...args);
   }
   regexp(...args) {
+    if (this.first) {
+      return new Assert().regexp(...args);
+    }
     if (_.isNil(this.value)) {
       args.splice(1, 0, 'regexp');
     } else {
@@ -175,27 +245,51 @@ class Assert {
     return this.execute(assert.typeOf, ...args);
   }
   integer(...args) {
+    if (this.first) {
+      return new Assert().integer(...args);
+    }
     return this.execute(isInteger, ...args);
   }
   zreo(...args) {
+    if (this.first) {
+      return new Assert().zreo(...args);
+    }
     return this.execute(isZreo, ...args);
   }
   positive(...args) {
+    if (this.first) {
+      return new Assert().positive(...args);
+    }
     return this.execute(isPositive, ...args);
   }
   negative(...args) {
+    if (this.first) {
+      return new Assert().negative(...args);
+    }
     return this.execute(isNegative, ...args);
   }
   typein(...args) {
+    if (this.first) {
+      return new Assert().typein(...args);
+    }
     return this.execute('typeOfIn', ...args);
   }
   digital(...args) {
+    if (this.first) {
+      return new Assert().digital(...args);
+    }
     return this.execute(isDigital, ...args);
   }
   decimal(...args) {
+    if (this.first) {
+      return new Assert().decimal(...args);
+    }
     return this.execute(decimal, ...args);
   }
   notMatch(...args) {
+    if (this.first) {
+      return new Assert().notMatch(...args);
+    }
     if (_.isNil(this.value)) {
       args[1] = getType(args[1]) === 'string' ? new RegExp(args[1], 'g') : args[1];
     } else {
@@ -204,6 +298,9 @@ class Assert {
     return this.execute(assert.notMatch, ...args);
   }
   matchCount(...args) {
+    if (this.first) {
+      return new Assert().matchCount(...args);
+    }
     if (_.isNil(this.value)) {
       args[1] = getType(args[1]) === 'string' ? new RegExp(args[1], 'g') : args[1];
     } else {
@@ -212,4 +309,4 @@ class Assert {
     return this.execute(matchCount, ...args);
   }
 }
-export default new Assert();
+export default new Assert(true);
